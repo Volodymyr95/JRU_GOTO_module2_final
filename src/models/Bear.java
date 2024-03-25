8e61ac544b5625b42cd8920120c1ca27ff60cbab
Volodymyr” <krokhmalyuk777@gmail.com>
2024-03-25 19:25:34 +0200

    Made bear runnable

--- src/models/Bear.java
+++ src/models/Bear.java
@@ -1,10 +1,15 @@
 package models;
 
-public class Bear {
+public class Bear implements Runnable {
 
     public static final String ICON = "\ud83d\udc3b";
 
     public void eat(Animal animal) {
+
+    }
+
+    @Override
+    public void run() {
 
     }
 }
